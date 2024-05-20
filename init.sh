83215e5f69f83bad2575419886ab3da2d6b1a942
Apatiga, Daniel <77174601+morphytron@users.noreply.github.com>
2024-05-19 22:19:14 -0400

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,39 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt install software-properties-common
+
+sudo apt-get install git
+sudo add-apt-repository main
+sudo add-apt-repository universe
+sudo add-apt-repository restricted
+sudo add-apt-repository multiverse
+sudo apt-get update
+sudo apt install ocl-icd-opencl-dev
+sudo apt-get install libmicrohttpd-dev libssl-dev cmake build-essential
+sudo apt-get install automake libtool make gcc
+sudo apt-get install libjansson-dev
+sudo apt-get update
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get update
+sudo apt-get install g++-5
+
+sudo apt-get upgrade
+
+sudo update-initramfs -u
+sudo apt-get install libopenblas-dev liblapack-dev
+sudo apt-get install linux-image-generic linux-image-extra-virtual
+sudo apt-get install linux-source linux-headers-generic
+
+sudo apt-get install libglu1-mesa libxi-dev libxmu-dev -y
+sudo apt-get — yes install build-essential
+sudo apt-get install python-pip python-dev -y
+sudo apt-get install python-numpy python-scipy -y
+sudo apt-get install libglu1-mesa libxi-dev libxmu-dev libglu1-mesa-dev
+sudo apt-get install apt-file && apt-file update
+
+sudo apt-get upgrade
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:george-edison55/cmake-3.x
+sudo apt-get update
+sudo apt-get install cmake
+sudo apt-get upgrade
