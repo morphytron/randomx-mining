104afd9a2b6e2e85978cc9e811c60590f055c745
Daniel Alexander Apatiga <daniel.apatiga@pickupfit.com>
2024-05-22 01:01:27 +0000

    Latest

--- shell/arm64/xmrig/start.sh
+++ shell/arm64/xmrig/start.sh
@@ -1,14 +1,14 @@
-export WORKER_ID=amd-desktop-1
+export WORKER_ID=odroid-1
 export HOSTNAME=localhost
 export CPU_PRIORITY=5
 export ALGO=rx/0
 export COIN=monero
-export THREADS=10
-export RANDOMX_MODE=fast
+export THREADS=1
+export RANDOMX_MODE=auto
 export POOL_URL=xmr-us-east1.nanopool.org:10343
-export CPU_MEMORY_POOL1=32 #memory is on chip
+export CPU_MEMORY_POOL=0 #memory is on chip
 export USER_ADDRESS=47ycYS8t79DSyYAYnpXGVJTSiGXAW7qnEVBXfwVfeQ9f4HYWLWEn2imV2WUZWTMPVbjTFaWFhDSxiUU22GscsCDnR7yWcEa.amd-desktop-1/dapatiga@gmail.com
-exec ./xmrig -t ${THREADS} --coin ${COIN} --a ${ALGO} --tls -o ${POOL_URL} \
+exec xmrig_/build/xmrig -t ${THREADS} --coin ${COIN} --a ${ALGO} --tls -o ${POOL_URL} \
 --keepalive --cpu-priority ${CPU_PRIORITY} \
 --nicehash \
 --api-worker-id \
