c49ac33e8330f219193bbe4a9672d8bc07dc0da0
Daniel Alexander Apatiga <daniel.apatiga@pickupfit.com>
2024-05-23 06:04:51 +0000

    Latest

--- shell/amd64/p2pool/start.sh
+++ shell/amd64/p2pool/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export WALLET=47ycYS8t79DSyYAYnpXGVJTSiGXAW7qnEVBXfwVfeQ9f4HYWLWEn2imV2WUZWTMPVbjTFaWFhDSxiUU22GscsCDnR7yWcEa
-p2pool --no-randomx --light-mode --host 0.0.0.0 --wallet ${WALLET} &
+exec p2pool --no-randomx --host 127.0.0.1 --wallet ${WALLET}
--- shell/amd64/xmrig/start.sh
+++ shell/amd64/xmrig/start.sh
@@ -10,7 +10,6 @@ export RANDOMX_MODE=fast
 export POOL_URL=localhost:3333
 export CPU_MEMORY_POOL=32 #memory is on chip
 export USER_ADDRESS=47ycYS8t79DSyYAYnpXGVJTSiGXAW7qnEVBXfwVfeQ9f4HYWLWEn2imV2WUZWTMPVbjTFaWFhDSxiUU22GscsCDnR7yWcEa.amd-desktop-1/dapatiga@gmail.com
-/root/xmr-mining/shell/amd64/xmrig/xmrig_/build/xmrig -t ${THREADS} --coin ${COIN} --a ${ALGO} --tls -o ${POOL_URL} \
+nohup /root/xmr-mining/shell/amd64/xmrig/xmrig_/build/xmrig -t ${THREADS} --coin ${COIN} --a ${ALGO} --tls -o ${POOL_URL} \
 --keepalive --cpu-priority ${CPU_PRIORITY} \
---api-worker-id \
-${WORKER_ID} --http-port 8069 --http-host ${HOSTNAME} --opencl &
+-api-worker-id ${WORKER_ID} --http-port 8069 --http-host ${HOSTNAME} --opencl
